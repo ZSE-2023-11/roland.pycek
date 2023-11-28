cb94a92e038718fbca55c0d081c9f9488e1b88df
C3bUlLiNi <rp.roland26@gmail.com>
2023-11-28 17:42:02 +0100

    zrobione3

--- index.php
+++ index.php
@@ -9,23 +9,31 @@
       <h1>Logowanie</h1>
       <form method="post">
         <div class="input">
-          <input type="text" name="login" placeholder="login" required>
+          <input type="text" name="username" placeholder="nazwa użytkownika" required>
         </div>
         <div class="input">
           <input type="text" name="password" placeholder="hasło" required>
         </div>
         <input type="submit" value="Zaloguj się"> <?php
-                if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if($_POST["login"] == "admin" && $_POST["password"] == "test")
-            {
-                echo "<a>Zalogowano</a>";
-            }
-            else 
-            {
-                echo "<a>Błędne dane logowania</a>";
-            }
-            }
-            ?>
+    $conn = new mysqli("127.0.0.1","admin","test","baza");
+
+if ($conn->connect_error) {
+    die("Błąd: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $query = "SELECT * FROM users WHERE username='" . $conn->real_escape_string($_POST["username"]) . "' AND password='" . $conn->real_escape_string($_POST["password"]) . "'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        echo "<a>Zalogowano</a>";
+    } 
+    else 
+    {
+        echo "<a>Niepoprawne dane logowania</a>";
+    }
+}
+?>
       </form>
     </div>
     <style>
